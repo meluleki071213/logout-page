175d70761f8d425d5972578d5674b659154e06d4
a233057 <meluleki.madlala@standardbank.co.za>
2023-06-22 23:11:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("logoutButton").addEventListener("click", function () {
+  // Perform logout functionality here (e.g., clearing session, redirecting to login page)
+
+  // Redirect to login page
+  window.location.href = "dinersclub:/";
+});
